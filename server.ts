9852578f2cac97e45e62b5793a0f84f5bf118c5f
vikerman <viks@angular.io>
2019-02-02 08:06:29 -0800

    Switch to undom from domino

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
-import * as domino from 'domino';
-import * as fs from 'fs';
-import {join} from 'path';
 import {getRendererFactory} from './server_renderer_factory';
+
+const createDocument = require('./undom/undom').default;
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 (global as any).ngDevMode = false;
@@ -10,24 +9,25 @@ import {getRendererFactory} from './server_renderer_factory';
 // webpack
 const {AppComponent, renderComponent, DomSanitizerImpl} = require('./dist/server/main');
 
-const MASTER_DOC: any = domino.createDocument('<app-root></app-root>');
-
 function render(): Document {
-  const doc: Document = MASTER_DOC.cloneNode(true);
+  const doc: Document = createDocument();
+  const host = doc.createElement('app-root');
+  doc.body.appendChild(host);
+  
   const rendererFactory = getRendererFactory(doc);
   const sanitizer = new DomSanitizerImpl(doc);
-  renderComponent(AppComponent, {rendererFactory, sanitizer});
+  renderComponent(AppComponent, {rendererFactory, host, sanitizer});
   return doc;
 }
 
 // Do a sanity check on the output.
 (function test() {
-  console.log(render().documentElement.outerHTML);
+  console.log(render().documentElement.toString());
 })();
 
 // Time X iterations.
 console.time('timer');
 for (let i = 0; i < 100000; i++) {
-  const output = render().documentElement.outerHTML;
+  const output = render().documentElement.toString();
 }
 console.timeEnd('timer');
